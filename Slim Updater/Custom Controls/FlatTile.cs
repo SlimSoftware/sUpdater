19aed88bc82ef1b690f8797481f5e406c1c5679e
Slim Software <markenco512@gmail.com>
2018-01-30 21:22:57 +0100

    Increase flatTile hover highlight

--- Slim Updater/Custom Controls/FlatTile.cs
+++ Slim Updater/Custom Controls/FlatTile.cs
@@ -14,11 +14,11 @@ namespace SlimUpdater
         }
 
         Color normalGreen = Color.FromArgb(0, 186, 0);
-        Color hoverGreen = Color.FromArgb(0, 196, 0);
+        Color hoverGreen = Color.FromArgb(0, 226, 0);
         Color normalOrange = Color.FromArgb(254, 124, 35);
-        Color hoverOrange = Color.FromArgb(254, 134, 35);
+        Color hoverOrange = Color.FromArgb(254, 164, 35);
         Color normalGrey = Color.FromArgb(141, 141, 141);
-        Color hoverGrey = Color.FromArgb(141, 151, 141);
+        Color hoverGrey = Color.FromArgb(141, 181, 141);
 
         // Required for the text to display correctly
         [Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
