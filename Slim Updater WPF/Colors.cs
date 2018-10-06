d385984ab4af4f80491a7de443702eaa552eb75a
SlimSoftware <markenco512@gmail.com>
2018-10-06 22:03:46 +0200

    Add FlatTile usercontrol and add startpage tiles

--- Slim Updater WPF/Colors.cs
+++ Slim Updater WPF/Colors.cs
@@ -5,10 +5,10 @@ namespace SlimUpdater
     public static class Colors
     {
         public static Brush normalGreenBrush = new SolidColorBrush(Color.FromRgb(0, 186, 0));
-        public static Brush hoverGreen = new SolidColorBrush(Color.FromRgb(0, 226, 0));
-        public static Brush normalOrange = new SolidColorBrush(Color.FromRgb(254, 124, 35));
-        public static Brush hoverOrange = new SolidColorBrush(Color.FromRgb(254, 164, 35));
-        public static Brush normalGrey = new SolidColorBrush(Color.FromRgb(141, 141, 141));
-        public static Brush hoverGrey = new SolidColorBrush(Color.FromRgb(141, 171, 141));
+        public static Brush hoverGreenBrush = new SolidColorBrush(Color.FromRgb(0, 226, 0));
+        public static Brush normalOrangeBrush = new SolidColorBrush(Color.FromRgb(254, 124, 35));
+        public static Brush hoverOrangeBrush = new SolidColorBrush(Color.FromRgb(254, 164, 35));
+        public static Brush normalGreyBrush = new SolidColorBrush(Color.FromRgb(141, 141, 141));
+        public static Brush hoverGreyBrush = new SolidColorBrush(Color.FromRgb(141, 171, 141));
     }
 }
