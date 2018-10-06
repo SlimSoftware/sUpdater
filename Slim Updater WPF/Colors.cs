1ae0c26bcd24b94fcce572374fb74b1164ac24f1
SlimSoftware <markenco512@gmail.com>
2018-10-06 21:02:11 +0200

    Add Colors class

---
+++ Slim Updater WPF/Colors.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media;
+
+namespace SlimUpdater
+{
+    public static class Colors
+    {
+        public static Brush normalGreenBrush = new SolidColorBrush(Color.FromRgb(0, 186, 0));
+        public static Brush hoverGreen = new SolidColorBrush(Color.FromRgb(0, 226, 0));
+        public static Brush normalOrange = new SolidColorBrush(Color.FromRgb(254, 124, 35));
+        public static Brush hoverOrange = new SolidColorBrush(Color.FromRgb(254, 164, 35));
+        public static Brush normalGrey = new SolidColorBrush(Color.FromRgb(141, 141, 141));
+        public static Brush hoverGrey = new SolidColorBrush(Color.FromRgb(141, 171, 141));
+    }
+}
