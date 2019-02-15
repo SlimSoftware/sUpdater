ee1ec93963a946961d9481507ce51d7553429dbd
SlimSoftware <markenco512@gmail.com>
2019-02-15 12:53:38 +0100

    Moved MainWindow content into StartPage and added Frame with custom nav UI

---
+++ Slim Updater WPF/StartPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SlimUpdater
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
