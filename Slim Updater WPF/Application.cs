d5e60186f37f0e5cfbddc7a02f3f83aa58e5c202
SlimSoftware <markenco512@gmail.com>
2019-03-05 14:53:12 +0100

    Added: Application class

---
+++ Slim Updater WPF/Application.cs
@@ -0,0 +1,29 @@
+﻿namespace SlimUpdater
+{
+    class Application
+    {
+        public string Name { get; set; }
+        public string LatestVersion { get; set; }
+        public string LocalVersion { get; set; }
+        public string Changelog { get; set; }
+        public string Description { get; set; }
+        public string Arch { get; set; }
+        public string Type { get; set; }
+        public string DL { get; set; }
+        public string SavePath { get; set; }
+        public string InstallSwitch { get; set; }
+
+        public Application(string name, string latestVersion, string localVersion, string arch,
+            string type, string installSwitch, string dl, string savePath = null)
+        {
+            Name = name;
+            LatestVersion = latestVersion;
+            LocalVersion = localVersion;
+            Arch = arch;
+            Type = type;
+            InstallSwitch = installSwitch;
+            DL = dl;
+            SavePath = savePath;
+        }
+    }
+}
