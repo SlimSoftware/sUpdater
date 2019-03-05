e41ec99696137be0abd77f916f74c29c1921804b
SlimSoftware <markenco512@gmail.com>
2019-03-05 14:53:42 +0100

    Added: ListItem UI to updateListView

--- Slim Updater WPF/UpdaterPage.xaml.cs
+++ Slim Updater WPF/UpdaterPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace SlimUpdater
 {
@@ -10,6 +11,19 @@ namespace SlimUpdater
         public UpdaterPage()
         {
             InitializeComponent();
+            List<User> items = new List<User>();
+            items.Add(new User() { Name = "John Doe", Age = 42 });
+            items.Add(new User() { Name = "Jane Doe", Age = 39 });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13 });
+            items.Add(new User() { Name = "this is a veeeeeerrrrrrryyyyy loooooooooooooonnnngggggg nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", Age = 18 });
+            updateListView.ItemsSource = items;
+        }
+
+        public class User
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
         }
     }
 }
