c0d66a08730b4d7c54b033d5d70d726c5ca1cb36
SlimSoftware <markenco512@gmail.com>
2019-03-11 18:11:33 +0100

    Add Apps class to share apps, updates and portable apps between Pages

---
+++ Slim Updater WPF/Apps.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace SlimUpdater
+{
+    /// <summary>
+    /// Holds the lists with information about apps and updates.
+    /// The class is used to share these lists between pages.
+    /// </summary>
+    public static class Apps
+    {
+        /// <summary>
+        /// A List that contains all apps, except for portable apps
+        /// </summary>
+        public static List<Application> Regular;
+
+        /// <summary>
+        /// A List that contains all updates that are currently available
+        /// </summary>
+        public static List<Application> Updates;
+
+        /// <summary>
+        /// A List that contains all Portable Apps
+        /// </summary>
+        public static List<Application> Portable;
+    }
+}
