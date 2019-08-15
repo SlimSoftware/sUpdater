3ebba650cafe1d13bb435eb3ced1fa9034b7731e
SlimSoftware <slimsoftware512@gmail.com>
2019-08-15 13:48:32 +0200

    Add missing siteLink click event

--- Slim Updater WPF/AboutPage.xaml.cs
+++ Slim Updater WPF/AboutPage.xaml.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Diagnostics;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Media;
 
 namespace SlimUpdater
 {
@@ -16,10 +14,15 @@ namespace SlimUpdater
             versionLabel.Content += Utilities.GetFriendlyVersion();
         }
 
-        private void CreditsLink_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void CreditsLink_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             CreditsPage creditsPage = new CreditsPage();
             NavigationService.Navigate(creditsPage);
+        }
+
+        private void SiteLink_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Process.Start("https://www.slimsoft.tk");
         }
     }
 }
