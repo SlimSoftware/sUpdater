2dd651fbac85b15cff29505e3c0057950dbb99d4
SlimSoftware <slimsoftware512@gmail.com>
2019-08-18 13:05:50 +0200

    Move download and install tasks to Application class so they can be reused

--- Slim Updater WPF/GetAppsPage.xaml.cs
+++ Slim Updater WPF/GetAppsPage.xaml.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Threading;
 
 namespace SlimUpdater
 {
@@ -101,55 +95,6 @@ namespace SlimUpdater
             GetNotInstalledApps();
         }
 
-        private async Task DownloadUpdate(Application app)
-        {
-            string fileName = Path.GetFileName(app.DownloadLink);
-            app.SavePath = Path.Combine(Settings.DataDir, fileName);
-            Log.Append("Saving to: " + app.SavePath, Log.LogLevel.INFO);
-
-            // Check if installer is already downloaded
-            if (!File.Exists(app.SavePath))
-            {
-                using (var wc = new WebClient())
-                {
-                    wc.DownloadProgressChanged += (s, args) =>
-                    {
-                        // Convert download size to mb
-                        double recievedSize = Math.Round(args.BytesReceived / 1024d / 1024d, 1);
-                        double totalSize = Math.Round(args.TotalBytesToReceive / 1024d / 1024d, 1);
-
-                        app.Progress = args.ProgressPercentage / 2;
-                        app.Status = string.Format(
-                            "Downloading... {0:0.0} MB/{1:0.0} MB", recievedSize, totalSize);                          
-                    };
-                    wc.DownloadFileCompleted += (s, args) =>
-                    {
-                        app.Status = "Download complete";
-                    };
-                    try
-                    {
-                        await wc.DownloadFileTaskAsync(new Uri(app.DownloadLink), app.SavePath);
-                    }
-                    catch (Exception ex)
-                    {
-                        Log.Append("An error occurred when attempting to download " +
-                            "the installer." + ex.Message, Log.LogLevel.ERROR);
-                        app.Status = ex.Message;
-
-                        if (File.Exists(app.SavePath))
-                        {
-                            File.Delete(app.SavePath);
-                        }
-                        return;
-                    }
-                }
-            }
-            else
-            {
-                app.Progress = 50;              
-            }
-        }
-
         private async void InstallButton_Click(object sender, RoutedEventArgs e)
         {
             bool installFailed = false;
@@ -180,7 +125,7 @@ namespace SlimUpdater
                     {
                         await Task.Delay(1000);
                     }
-                    tasks.Add(DownloadUpdate(app));
+                    tasks.Add(app.Download());
                 }
                 await Task.WhenAll(tasks);
 
@@ -189,81 +134,11 @@ namespace SlimUpdater
                 foreach (Application app in getAppsListView.SelectedItems)
                 {
                     currentApp++;
-                    launchInstaller:
                     if (File.Exists(app.SavePath))
                     {
                         Log.Append(string.Format("Installing {0} ({1} of {2}) ...", app.Name,
                             currentApp, getAppsListView.SelectedItems.Count), Log.LogLevel.INFO);
-                        using (var p = new Process())
-                        {
-                            if (app.DownloadLink.EndsWith(".exe"))
-                            {
-                                p.StartInfo.FileName = app.SavePath;
-                                p.StartInfo.UseShellExecute = true;
-                                p.StartInfo.Verb = "runas";
-                                p.StartInfo.Arguments = app.InstallSwitch;
-                            }
-                            else if (app.DownloadLink.EndsWith(".msi"))
-                            {
-                                p.StartInfo.FileName = "msiexec";
-                                p.StartInfo.UseShellExecute = true;
-                                p.StartInfo.Verb = "runas";
-                                p.StartInfo.Arguments = "\"" + app.InstallSwitch + "\""
-                                    + " " + app.SavePath;
-                            }
-                            try
-                            {
-                                p.Start();
-                            }
-                            catch (Exception)
-                            {
-                                var result = MessageBox.Show(
-                                    "Lauching the installer failed. \nWould you like to try again?",
-                                    "Error", MessageBoxButton.YesNo, MessageBoxImage.Question);
-                                Log.Append("Launching the installer failed. Asking user for retry.",
-                                    Log.LogLevel.INFO);
-                                if (result == MessageBoxResult.Yes)
-                                {
-                                    Log.Append("User chose yes.", Log.LogLevel.INFO);
-                                    goto launchInstaller;
-                                }
-                                else
-                                {
-                                    Log.Append("User chose no. Skipping this app.", Log.LogLevel.INFO);
-                                    continue;
-                                }
-                            }
-
-                            // Restore back focus to the MainWindow
-                            System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault().Focus();
-
-                            app.Status = "Installing...";
-                            app.IsWaiting = true;
-
-                            // Wait on a separate thread for the process to exit so the GUI thread does not get blocked
-                            await Task.Run(() =>
-                            {
-                                p.WaitForExit();
-                            });
-
-                            if (p.ExitCode == 0)
-                            {
-                                Log.Append("Installer exited with exit code 0.", Log.LogLevel.INFO);
-                                File.Delete(app.SavePath);
-                                app.Status = "Install complete";
-                                app.IsWaiting = false;
-                                app.Progress = 100;
-                            }
-                            if (p.ExitCode != 0)
-                            {
-                                Log.Append(string.Format("Installation failed. Installer exited with " +
-                                    "exit code {0}.", p.ExitCode), Log.LogLevel.ERROR);
-                                app.Status = String.Format(
-                                    "Install failed. Exit code: {0}", p.ExitCode);
-                                app.Progress = 0;
-                                app.IsWaiting = false;
-                            }
-                        }
+                        await app.Install();
                     }
                 }
 
