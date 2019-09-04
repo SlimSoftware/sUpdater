dd58fbe3232a3f9213be015447dcd35848a2e86d
SlimSoftware <markenco512@gmail.com>
2019-09-04 09:10:52 +0200

    Add PortableApp class

---
+++ sUpdater/PortableApp.cs
@@ -0,0 +1,28 @@
+﻿namespace sUpdater
+{
+    class PortableApp
+    {
+        public string Name { get; set; }
+        public string LatestVersion { get; set; }
+        public string LocalVersion { get; set; }
+        public string Status { get; set; }
+        public string Arch { get; set; }
+        public string DL { get; set; }
+        public string ExtractMode { get; set; }
+        public string SavePath { get; set; }
+        public string Launch { get; set; }
+
+        public PortableApp(string name, string latestVersion, string localVersion, string arch,
+            string launch, string dl, string extractMode, string savePath = null)
+        {
+            Name = name;
+            LatestVersion = latestVersion;
+            LocalVersion = localVersion;
+            Arch = arch;
+            Launch = launch;
+            DL = dl;
+            ExtractMode = extractMode;
+            SavePath = savePath;
+        }
+    }
+}
