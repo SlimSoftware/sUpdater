14d6855aad7672de4e77cd4f4c21f513fb5e9edf
SlimSoftware <slimsoftware512@gmail.com>
2020-07-18 14:46:18 +0200

    Add Inno Setup files

---
+++ sUpdater/Installer/upgrade.bat
@@ -0,0 +1,5 @@
+mkdir %appdata%\Slim Software\sUpdater
+copy %appdata%\Slim Software\Slim Updater\Settings.xml %appdata%\Slim Software\sUpdater /y
+rmdir %appdata%\Slim Software\Slim Updater /s /q
+rmdir %appdata%\Slim Software\Slim Updater /s /q
+rmdir %ProgramFiles%\Slim Software\Slim Updater
