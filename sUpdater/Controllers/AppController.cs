dfa8099c7f705c01f98c9034140338899c6764c9
SlimSoftware <slimsoftware512@gmail.com>
2020-09-10 09:51:40 +0200

    Add AppController

---
+++ sUpdater/Controllers/AppController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace sUpdater.Controllers
+{
+    public static class AppController
+    {
+        public static async Task<Application> GetDetectInfo()
+        {
+            using (var response = await Utilities.HttpClient.GetAsync("apps/?f=id,arch,version,regkey,regvalue,exe_path"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    Application app = await response.Content.ReadAsAsync<Application>();
+                    return app;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
