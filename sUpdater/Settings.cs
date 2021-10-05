3dd0ae077e3941dd2ca70c147e59637fe9299271
SlimSoftware <slimsoftware512@gmail.com>
2021-10-05 19:54:04 +0200

    Add default setting values for DataDir and PortableAppDir (fixes #13)

--- sUpdater/Settings.cs
+++ sUpdater/Settings.cs
@@ -1,11 +1,18 @@
-﻿namespace sUpdater.Models
+﻿using System;
+using System.IO;
+
+namespace sUpdater.Models
 {
     public class Settings
     {
         public bool MinimizeToTray { get; set; } = true;
         public string DefenitionURL { get; set; }
-        public string DataDir { get; set; }
-        public string PortableAppDir { get; set; }
+
+        public string DataDir { get; set; } = Path.Combine(Environment.GetFolderPath(
+            Environment.SpecialFolder.ApplicationData), @"Slim Software\sUpdater");
+        public string PortableAppDir { get; set; } = Path.Combine(Environment.GetFolderPath(
+            Environment.SpecialFolder.MyDocuments), "Portable Apps");
+
         public string NotifiedUpdates { get; set; }
     }
 }
